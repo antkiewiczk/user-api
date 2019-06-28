d7a120b745124aead30e898eaad7f4bd4e5b0cf3
Kamil Antkiewicz <antkiewiczk@gmail.com>
2019-06-28 12:59:40 +0200

    Updates

---
+++ precache-manifest.cad7c7580683ce31e3f56c15286b4102.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a3df5cda148cfa0d700df1b8cd5fd1d",
+    "url": "/user-api/index.html"
+  },
+  {
+    "revision": "e50862b6dbb1e0ae4d21",
+    "url": "/user-api/static/css/main.f608b072.chunk.css"
+  },
+  {
+    "revision": "e6ee0d9fea2014f1cfdd",
+    "url": "/user-api/static/js/2.4a9c6eff.chunk.js"
+  },
+  {
+    "revision": "e50862b6dbb1e0ae4d21",
+    "url": "/user-api/static/js/main.f7101de5.chunk.js"
+  },
+  {
+    "revision": "0ccca11f5db789214761",
+    "url": "/user-api/static/js/runtime~main.f78a9db8.js"
+  }
+]);
